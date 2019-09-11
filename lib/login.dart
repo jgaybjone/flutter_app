dabaeb33f29784132517a1a9bc3e406c474faed5
jungang <82099359@qq.com>
2019-09-11 13:50:58 +0800

    12

--- lib/login.dart
+++ lib/login.dart
@@ -4,7 +4,8 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var head = Container(
-        padding: EdgeInsets.fromLTRB(0, 60, 0, 60),
+        padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+//        height: 100,
         child: Center(
           child: Image.asset(
             'images/portrait.png',
@@ -37,7 +38,7 @@ class Login extends StatelessWidget {
     );
 
     var loginBtn = Container(
-      padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
       child: RaisedButton(
         onPressed: () {
           print('触碰了登录按钮');
@@ -56,18 +57,97 @@ class Login extends StatelessWidget {
           print('触碰了注册按钮');
           Navigator.pop(context);
         },
-        padding: EdgeInsets.fromLTRB(10, 6, 10, 6),
+//        padding: EdgeInsets.fromLTRB(10, 6, 10, 6),
         color: Colors.blue,
         textColor: Colors.white,
         child: Text('注册'),
       ),
     );
 
-    final rows = ListView(
-      physics: const NeverScrollableScrollPhysics(),
-      children: <Widget>[head, mobile, password, loginBtn, registBtn],
+    var resetPwd2 = Align(
+      alignment: Alignment.bottomCenter,
+      child: FlatButton(
+        onPressed: () => print('点击了重置密码'),
+        child: Text(
+          '重置密码',
+          style: TextStyle(fontSize: 14, color: Colors.grey),
+        ),
+      ),
     );
 
-    return Scaffold(body: rows);
+    var wechatLoginBtn = Center(
+      child: IconButton(
+          icon: ImageIcon(AssetImage('icons/wechat.png')),
+          onPressed: () {
+            print('微信登录');
+          }),
+    );
+
+    var body = Column(
+      children: <Widget>[
+        Expanded(
+          child: head,
+          flex: 3,
+        ),
+        Container(
+          child: Column(
+            children: <Widget>[mobile, password],
+          ),
+        ),
+        Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: loginBtn,
+              )
+            ],
+          ),
+        ),
+        Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: registBtn,
+              )
+            ],
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text('微信登录'),
+                ),
+                IconButton(
+                    icon: ImageIcon(AssetImage('icons/wechat.png')),
+                    iconSize: 40,
+                    color: Colors.grey,
+                    onPressed: () {
+                      print('微信登录');
+                    }),
+              ],
+            )),
+        Expanded(
+          child: resetPwd2,
+          flex: 1,
+        )
+      ],
+    );
+
+    return Scaffold(
+      body: body,
+//      floatingActionButton: FlatButton(
+//        onPressed: () => print('点击了重置密码'),
+//        child: Text(
+//          '重置密码',
+//          style: TextStyle(fontSize: 14, color: Colors.grey),
+//        ),
+//      ),
+//      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//      floatingActionButton: <Widget>[resetPwd],
+    );
   }
 }
